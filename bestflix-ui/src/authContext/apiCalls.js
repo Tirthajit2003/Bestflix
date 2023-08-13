b0ae8947398c32a71134a76826c3b4f183d4a625
Tirthajit Boral <121869956+Tirthajit2003@users.noreply.github.com>
2023-08-13 12:02:32 +0530

    Update apiCalls.js

--- bestflix-ui/src/authContext/apiCalls.js
+++ bestflix-ui/src/authContext/apiCalls.js
@@ -3,6 +3,7 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
+  axios.defaults.withCredentials=true;
   try {
     const res = await axios.post("https://bestflix-api.vercel.app/auth/login", user);
     dispatch(loginSuccess(res.data));
